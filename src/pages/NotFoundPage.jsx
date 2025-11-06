6d18e23641906a40fff0485a3dd623104f0ae3e9
Альберт Микаелян <110053567+Ddyadz01@users.noreply.github.com>
2025-11-06 22:56:39 +0300

    Create NotFoundPage.jsx

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const NotFound = () => {
+  return (
+    <div className="">
+    <h1>Error 404</h1>
+<p>Страница не найдена</p>
+    </div>
+  )
+}
+
+export default NotFound
